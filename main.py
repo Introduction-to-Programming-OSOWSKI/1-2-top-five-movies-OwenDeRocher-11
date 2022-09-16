8c7ae3be4335af77a3ae8609b493e33d16e1ed44
OwenDeRocher-11 <817934@mystma.org>
2022-09-16 14:13:51 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFIveMovies(a,b,c,d,e):
+    return a + " " + b + " " + c + " " + d +" " + e
+
+print(topFIveMovies("Shrek", "Shrek 2", "Shrek 3", "Shrek 4", "Shrek 5"))
